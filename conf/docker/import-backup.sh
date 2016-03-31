7cd145a5bbc5049f5409e086422e77aa8ecaea68
Ben Kutil <ben@make-things.com>
2016-03-30 20:45:26 -0400

    :zap: update data out

--- conf/docker/import-backup.sh
+++ conf/docker/import-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --volumes-from jhuhopkinslocaldev_data_1 -v /vagrant/Documents/projects/jhu-hopkinslocal-dev/shared:/shared ubuntu tar cvf /shared/uploads.tar -C /app
+docker run --volumes-from jhuhopkinslocaldev_data_1 -v /vagrant/jhu-hopkinslocal-dev/shared:/shared ubuntu tar cvf /shared/uploads.tar -C /app
